565365090b12333131114b56248f4a66ab7a8755
x0dyR <manoshki.dan4ik@gmail.com>
2024-11-20 01:51:23 +0500

    Update PathGoalPoint.cs

--- Assets/_Scripts/Services/PathGoalPoint.cs
+++ Assets/_Scripts/Services/PathGoalPoint.cs
@@ -8,14 +8,14 @@ public class PathGoalPoint
 
     public PathGoalPoint(PathGoalObject visualObject)
     {
-        _visualObject = Object.Instantiate(visualObject);
+        _visualObject = Object.Instantiate(visualObject, Vector3.down, Quaternion.identity, null);
     }
 
     public void MoveTo(Vector3 position)
     {
         if (_lastPosition == position)
             return;
-        
+
         _lastPosition = position;
         _visualObject.transform.position = position;
     }
